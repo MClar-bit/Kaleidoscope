d813e500772cf120d13c15258ef5b22b24e3258f
MClar-bit <clar.miu@gmail.com>
2025-02-20 10:34:36 +0900

    Redo Initial Commit
    I misunderstood how github works. I redid my initial commit.

--- sketch.js
+++ sketch.js
@@ -3,23 +3,41 @@ let mover;
 let symmetry = 6;
 let angle = 360 / symmetry;
 
+let lineArr = [];
+
+let r = 0;
+let g=0;
+let b= 0;
+let rdown;
+
 function setup() {
   angleMode(DEGREES);
   createCanvas(600, 500);
-  background(255);
+  background(0);
   mover = new Mover();
 }
 
 function draw() {
+  translate(random(-5, 5), random(-5, 5));
   mover.update();
   mover.show();
   mover.checkEdges();
+  if (r==255){
+    rdown = true;
+  } else if(r==0) {
+    rdown = false;
+  }
+  if (rdown == true) {
+    r--;
+  } else {
+    r++;
+  }
 
 }
 
 class Mover {
   constructor() {
-    this.position = createVector(random(width), random(height)); 
+    this.position = createVector(random(width), random(height));
     this.velocity = createVector();
     this.acceleration = createVector();
     this.topSpeed = 5;
@@ -36,21 +54,21 @@ class Mover {
   }
 
   show() {
-    stroke(0);
     strokeWeight(2);
     translate(width / 2, height / 2);
-    
-    
+
+
     if (this.edgeWrap==false){
       this.reflect();
     }
     this.prevPosition.set(this.position);
     this.edgeWrap = false
   }
-  
+
   reflect(){
      for (let i = 0; i < symmetry; i++) {
-        rotate(angle);
+       rotate(angle);
+       stroke(r, 0, 100)
        line(this.prevPosition.x-width/2, this.prevPosition.y-height / 2, this.position.x-width/2, this.position.y-height / 2);
 
         push();
